e110e3bd165e9c9ef48e00f37e54246626538274
Clayton Knittel <cknit1999@gmail.com>
2018-09-16 00:39:32 -0500

    Initial Commit

---
+++ math/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  math
+//
+//  Created by Clayton Knittel on 9/16/18.
+//  Copyright © 2018 Clayton Knittel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
